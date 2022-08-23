b5f885de156df46e5b705aeeefe199c920ffd502
cjb2k1 <67792413+cjb2k1@users.noreply.github.com>
2022-08-23 07:29:37 +0100

    logicAppURI converted to URI

--- AF-PayPalIPN.cs
+++ AF-PayPalIPN.cs
@@ -18,8 +18,10 @@ namespace Lighthouse.AF_PayPalIPN
     {
         private static HttpClient httpClient = new HttpClient();
 
-        private static string logicAppUri = "https://shorturl.at/kQUXZ";
-
+        //private static string logicAppUri = "https://shorturl.at/kQUXZ";
+        //private static string logicAppUri = @"https://prod-59.northeurope.logic.azure.com:443/workflows/d9697466474141b3ba0018f78b5c75c9/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=h6NznOI6qVjcCG1WZ9OhEzrP0ycJQ1rr815S4DlJE-4";
+        private static Uri logicAppUri = new Uri("https://prod-59.northeurope.logic.azure.com:443/workflows/d9697466474141b3ba0018f78b5c75c9/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=h6NznOI6qVjcCG1WZ9OhEzrP0ycJQ1rr815S4DlJE-4");
+        
         [FunctionName("AF_PayPalIPN")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req, ILogger log)
